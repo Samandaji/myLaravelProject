0cd50e577ab1dfe41c6e7281030d725813d0f941
samandaji <contactabubakarusman@gmail.com>
2025-02-22 02:55:22 -0800

    last update

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,6 +6,14 @@
     <title></title>
 </head>
 <body>
-    <h1>Admin Page</h1>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Admin Dashboard') }}
+        </h2>
+    </x-slot>
+
+    
+</x-app-layout>
 </body>
 </html>
